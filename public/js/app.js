711ad3d4c6f88f531546d2c7a370ef03ceaf27fa
Aaron Schwartz <aschwartz628@gmail.com>
2016-09-27 20:58:33 -0400

    Add beginning of pomodoro timer

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,12 @@
+(function() {
+  //initialize variables
+  var startButton = $('#start');
 
+  //main functionality
+  startButton.on('click', countdown);
+
+  //function definitions
+  function countdown(){
+      alert("It's the final countdown!!!");
+  }
+}());
