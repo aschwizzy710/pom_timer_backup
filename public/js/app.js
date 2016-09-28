9e5298ad6d32962c3d5d62a9301c2b44e50e9875
Aaron Schwartz <aschwartz628@gmail.com>
2016-09-28 19:42:30 -0400

    Added seconds counter functionability to pomodoro counter

--- public/js/app.js
+++ public/js/app.js
@@ -1,12 +1,44 @@
 (function() {
   //initialize variables
-  var startButton = $('#start');
+  var startButton= $("#start");
+  var seconds = $("#seconds");
+
+
 
   //main functionality
-  startButton.on('click', countdown);
+  startButton.on("click", countdown);
 
-  //function definitions
+
+
+
+
+  //function definition
   function countdown(){
-      alert("It's the final countdown!!!");
+    var secondsText = seconds.text ();
+    var secondsTextAsNumber = parseInt(secondsText);
+    if (secondsTextAsNumber === 0) {
+      seconds.text ("59");
+      //then change seconds text to 59
+  } else {
+
+    var decreasedSecondsAsNumberByOne = secondsTextAsNumber - 1;
+    var padSecondsTextAsNumber = pad(decreasedSecondsAsNumberByOne);
+    seconds.text(padSecondsTextAsNumber);
   }
+    // var secondsValue = parseInt(seconds.text());
+    // console.log(secondsValue);
+    // seconds.text(pad(secondsValue - 1));
+    //transforms letter to text
+  }
+
+    function pad (num){
+        if(num < 10){
+          //spit out number with a leading 0
+          return "0" + num;
+
+      } else {
+        //spit out the original number
+        return num;
+      }
+    }
 }());
