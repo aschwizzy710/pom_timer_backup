021252da2f2de21fdc7f8ac5ceae8d1d8759d1bd
Aaron Schwartz <aschwartz628@gmail.com>
2016-09-28 20:44:05 -0400

    Make timer stop at 00:00

--- public/js/app.js
+++ public/js/app.js
@@ -4,11 +4,16 @@
   var startButton= $("#start");
   var seconds = $("#seconds");
   var minutes = $("#minutes");
-
+  var timerInterval;
   //main functionality
-  startButton.on("click", countdown);
+  startButton.on("click", startTimer);
 
   //function definition
+  function startTimer (){
+      if(!timerInterval){
+          timerInterval = setInterval(countdown, 1000);
+      }
+  }
   function countdown(){
     var secondsText = seconds.text ();
     var secondsTextAsNumber = parseInt(secondsText);
