66a4088a617d93a1002f2efb92a62c95a6fc2781
Aaron Schwartz <aschwartz628@gmail.com>
2016-09-29 18:53:39 -0400

    added break button and disabled start when the timer expires

--- public/js/app.js
+++ public/js/app.js
@@ -4,6 +4,7 @@
   var startButton= $("#start");
   var seconds = $("#seconds");
   var minutes = $("#minutes");
+  var breakButton = $('#break');
   var timerInterval;
   //main functionality
   startButton.on("click", startTimer);
@@ -24,7 +25,12 @@
     //console.log(typeof secondsTextAsNumber);
     if (minutesTextAsNumber === 0 && secondsTextAsNumber === 0){
       //stop!
-      return; //temporary
+      clearInterval(timerInterval); //this will stop the timer
+      //disable the start button
+      startButton.attr('disabled', true);
+      //unhide the break button
+      breakButton.show();
+      return;
     }
     if (secondsTextAsNumber === 0) {
       if (minutesTextAsNumber !== 0){
